071f0574584d8cdc145149dc321a1b219d661883
samverh <samverhezen@hotmail.com>
2023-10-07 13:03:07 +0200

    vercel setup

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-**0&bj1-%nb1lal^vy-n5@^98=@-agt1*g9rf9-r@12@&4i_gw'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
