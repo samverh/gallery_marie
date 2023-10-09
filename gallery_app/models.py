d525667d0a2864151668199535162fce6d452af5
samverh <samverhezen@hotmail.com>
2023-10-09 11:49:08 +0200

    db config

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Image(models.Model):
     title = models.CharField(max_length=20)
-    set = models.CharField(max_length=20)
+    set = models.CharField(max_length=20, null=True, blank=True)
     photo = models.ImageField(upload_to='images')
--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,13 @@ WSGI_APPLICATION = 'gallery_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES['default'] =  dj_database_url.config()
 
 
 
@@ -126,9 +128,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
