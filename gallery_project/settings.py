88fe01fd8136644b92688c80bb4b4edcc9be6883
samverh <samverhezen@hotmail.com>
2023-10-09 11:54:30 +0200

    db test

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -85,7 +85,21 @@ WSGI_APPLICATION = 'gallery_project.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+
+        'NAME': BASE_DIR / 'db.sqlite3',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# DATABASES['default'] =  dj_database_url.config()
 
 
 
